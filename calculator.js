746a62ffcd887d4ad2ff34e45f8b567ac96ac4af
afifatussalma <93476708+afifatussalma@users.noreply.github.com>
2021-10-31 21:19:08 +0700

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,19 @@
+let result=document.getElementById("inputext");
+let calculate=(number)=> {
+    result.value+=number;
+}
+let Result=()=> {
+    try{
+        result.value=eval(result.value);
+    }
+    catch(err){
+        alert("Enter the Valid Value");
+    }
+}
+
+function clr(){
+    result.value=" ";
+}
+function del(){
+    result.value=result.value.slice(0,-1)
+}
